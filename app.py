cd9d357f6e4d6070aeac1e46aee8a81d54a8139f
Vikram(SJCE) <m8vikram@gmail.com>
2022-08-14 13:26:18 +0530

    Error archives added and something more

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def sendemail():
 		message = request.form['message']
 
 		# Set your credentials
-		yourEmail = "suraj@geeksforgeeks.org"
+		yourEmail = "m8vikram@gmail.com"
 		yourPassword = "########"
 
 		# Logging in to our email account
