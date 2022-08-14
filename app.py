7df8098e1289adedabe78b78892a00a51d6a1393
Vikram(SJCE) <m8vikram@gmail.com>
2022-08-14 11:32:26 +0530

    flask

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
-'''
-Source:
+from flask import Flask, render_template, request, url_for, redirect
+from email.mime.text import MIMEText
+import smtplib
+from email.message import EmailMessage
+app = Flask(__name__)
 
-Author: Raja CSP
+@app.route("/")
+def index():
+	return render_template("index.html")
 
+@app.route("/sendemail/", methods=['POST'])
+def sendemail():
+	if request.method == "POST":
+		name = request.form['name']
+		subject = request.form['Subject']
+		email = request.form['_replyto']
+		message = request.form['message']
 
-'''
-from flask import Flask,render_template
-import random
-import json
+		# Set your credentials
+		yourEmail = "suraj@geeksforgeeks.org"
+		yourPassword = "########"
 
-app  = Flask(__name__)
-PORT = 3009
+		# Logging in to our email account
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.ehlo()
+		server.starttls()
+		server.login(yourEmail, yourPassword)
 
-    
-@app.route("/", methods=["GET","POST"])
-def startpy():
+		# Sender's and Receiver's email address
+		msg = EmailMessage()
+		msg.set_content("First Name : "+str(name)
+						+"\nEmail : "+str(email)
+						+"\nSubject : "+str(subject)
+						+"\nMessage : "+str(message))
+		msg['To'] = email
+		msg['From'] = yourEmail
+		msg['Subject'] = subject
 
-    result = {
-
-        "Greetings" : "Tactlabs welcomes you"
-    }
-
-    return render_template("index.html", result = result)
-
+		# Send the message via our own SMTP server.
+		try:
+			# sending an email
+			server.send_message(msg)
+			print("Send")
+		except:
+			print("Fail to Send")
+			pass
+			
+	return redirect('/')
 
 if __name__ == "__main__":
-    app.run( debug = True,host="0.0.0.0",port = PORT)
-    
+	app.run(debug=True)
